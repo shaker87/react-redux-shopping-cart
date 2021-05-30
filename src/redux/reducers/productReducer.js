9f46e90bfbe30793d09422ef00ee545346c0bc28
shaker <shaker.hossain87@gmail.com>
2021-05-30 23:03:07 +0600

    fix issue

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -13,51 +13,55 @@ const productReducer = (state = initialState, action) => {
         ...state,
       };
     case Types.ADD_TO_CART:
-      let cloneProduct = [...state.products]
-   
-      let cloneCart = [...state.cart];
-      let uniqueItem = cloneCart.find(item => item.id === action.payload.id)
-      if(uniqueItem){
-          uniqueItem.qty +=1
+      const cloneProduct = [...state.products];
+
+      const cloneCart = [...state.cart];
+      const uniqueItem = cloneCart.find((item) => item.id === action.payload.id);
+      if (uniqueItem) {
+        uniqueItem.qty += 1;
       }
-      if(!uniqueItem){
-          cloneCart.push(action.payload)
+      if (!uniqueItem) {
+        cloneCart.push(action.payload);
       }
- 
+
       return {
         ...state,
         products: cloneProduct,
         cart: cloneCart,
       };
 
-      case Types.HANDLE_DECREMENT_ITEM:
-          const cloneCartDecrement = [...state.cart]
-          
-          let decrementItem = cloneCartDecrement.find(item => item.id === action.payload.id)
-          if(decrementItem){
-            decrementItem.qty -=1;
-          }
-          if(decrementItem.qty === 0){
-              const itemIndex = cloneCartDecrement.findIndex(item => item.id ===action.payload.id)
-            cloneCartDecrement.splice(itemIndex, 1)
-          }
-        return {
-          ...state,
-          cart:cloneCartDecrement
-        };
+    case Types.HANDLE_DECREMENT_ITEM:
+      const cloneCartDecrement = [...state.cart];
 
-        
+      const decrementItem = cloneCartDecrement.find(
+        (item) => item.id === action.payload.id
+      );
+      if (decrementItem) {
+        decrementItem.qty -= 1;
+      }
+      if (decrementItem.qty === 0) {
+        const itemIndex = cloneCartDecrement.findIndex(
+          (item) => item.id === action.payload.id
+        );
+        cloneCartDecrement.splice(itemIndex, 1);
+      }
+      return {
+        ...state,
+        cart: cloneCartDecrement,
+      };
 
-        case Types.HANDLE_INCREMENT_ITEM:
-            const cloneCartIncrement = [...state.cart]
-            let incrementItem = cloneCartIncrement.find(item => item.id === action.payload.id)
-            if(incrementItem){
-                incrementItem.qty +=1;
-            }
-          return {
-            ...state,
-            cart:cloneCartIncrement
-          };
+    case Types.HANDLE_INCREMENT_ITEM:
+      const cloneCartIncrement = [...state.cart];
+      const incrementItem = cloneCartIncrement.find(
+        (item) => item.id === action.payload.id
+      );
+      if (incrementItem) {
+        incrementItem.qty += 1;
+      }
+      return {
+        ...state,
+        cart: cloneCartIncrement,
+      };
 
     default:
       break;
